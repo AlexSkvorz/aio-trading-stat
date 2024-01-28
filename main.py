bfcd787544a554eccd1a4b9e7930a69b7c1af757
Nikita Korobov <riper5218@yandex.ru>
2024-01-28 23:14:11 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from aiogram import Bot, Dispatcher
-from aiogram.filters.command import Command
-from config.bot_config_example import BOT_CONFIG
+import asyncio
 
-bot = Bot(BOT_CONFIG['TOKEN'])
-dispatcher = Dispatcher()
-
-
-@dispatcher.message(Command("start"))
-async def send_welcome_message(message):
-    await message.answer(f"Привет, <b><i>{message.from_user.first_name}</i></b>", parse_mode="HTML")
+from aiogram import Dispatcher, Bot
+from config.bot_config import BOT_CONFIG
+from telegram_bot.handlers import (start_command_handler, message_handler)
 
 
-@dispatcher.message()
-async def send_insult(message):
-    await message.answer("Ваня лох")
+async def main():
+    bot = Bot(BOT_CONFIG['TOKEN'])
+    dp = Dispatcher()
 
-dispatcher.run_polling(bot)
+    dp.include_routers(
+        start_command_handler.router,
+        message_handler.router,
+
+    )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
