64059083cc0084214f4883a62b6a17f7f7d13d18
Nikita Korobov <riper5218@yandex.ru>
2024-01-28 23:14:11 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from aiogram import Dispatcher, Bot
+from config.bot_config import BOT_CONFIG
+from telegram_bot.handlers import (start_command_handler, message_handler)
+
+
+async def main():
+    bot = Bot(BOT_CONFIG['TOKEN'])
+    dp = Dispatcher()
+
+    dp.include_routers(
+        start_command_handler.router,
+        message_handler.router,
+
+    )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
