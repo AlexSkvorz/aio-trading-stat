99fb27e9da22601a8398221143dc7f7a5682b986
Nikita Korobov <riper5218@yandex.ru>
2024-01-29 15:02:17 +0300

    changed bot_config to bot_config_example

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters.command import Command
-from config.bot_config import BOT_CONFIG
+from config.bot_config_example import BOT_CONFIG
 
 bot = Bot(BOT_CONFIG['TOKEN'])
 dispatcher = Dispatcher()
 
+
 @dispatcher.message(Command("start"))
 async def send_welcome_message(message):
     await message.answer(f"Привет, <b><i>{message.from_user.first_name}</i></b>", parse_mode="HTML")
+
 
 @dispatcher.message()
 async def send_insult(message):
