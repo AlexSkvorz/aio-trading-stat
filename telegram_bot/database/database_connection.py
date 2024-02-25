fd74bd5a27bbbda27cb0f171d5762b2c23cc593c
Eiritel <zhenya.mikhailov.02@mail.ru>
2024-02-25 11:22:29 +0300

    refactor: change structure of database

--- telegram_bot/database/database_connection.py
+++ telegram_bot/database/database_connection.py
@@ -1,24 +1,33 @@
-from pydantic_settings import BaseSettings
-from config.database_config import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import asyncio
+import socket
+from sqlalchemy import MetaData
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from config.database_config import POSTGRES_CONNECTION
+from telegram_bot.database.database_model import Base
 
 
-class Settings(BaseSettings):
-    DB_HOST: str = DB_HOST
-    DB_PORT: int = DB_PORT
-    DB_USER: str = DB_USER
-    DB_PASS: str = DB_PASS
-    DB_NAME: str = DB_NAME
+class DatabaseManager:
+    def __init__(self, pg_connection):
+        self.engine = create_async_engine(pg_connection, echo=True)
+        self.Session = async_sessionmaker(self.engine, expire_on_commit=False, echo=True, class_=AsyncSession)
+        self.metadata = MetaData()
 
-    @property
-    def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    async def init_models(self):
+        try:
+            async with self.engine.begin() as conn:
+                await conn.run_sync(Base.metadata.create_all)
+            self.engine.echo = True
+        except socket.gaierror:
+            print("Не удалось разрешить имя хоста в строке подключения к базе данных")
+            raise
+
+    async def async_init(self):
+        await self.init_models()
 
 
-settings = Settings()
-
-async_engine = create_async_engine(
-    url=settings.DATABASE_URL_asyncpg,
-)
-
-async_session_factory = async_sessionmaker(async_engine)
+try:
+    db_manager = DatabaseManager(POSTGRES_CONNECTION)
+    asyncio.run(db_manager.init_models())
+except (OperationalError, socket.gaierror):
+    print("Не удалось инициализировать DatabaseManager")
