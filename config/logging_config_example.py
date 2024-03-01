e2001616ed7290097fb9b5626e7a4b3fb4eb056c
Eiritel <zhenya.mikhailov.02@mail.ru>
2024-03-01 17:06:00 +0300

    refactor: change structure of logging

--- config/logging_config_example.py
+++ config/logging_config_example.py
@@ -1,6 +1,1 @@
-# log_dir - Gets the absolute path to the "logs" directory in the project root
-# log_file - Gets the absolute path to file, containing "yourname_file.log" in the "logs" directory
-import os
-
-log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs')
-log_file = os.path.join(log_dir, 'your_file.log')
+LOGFILE = 'path/fatal_errors.log'
